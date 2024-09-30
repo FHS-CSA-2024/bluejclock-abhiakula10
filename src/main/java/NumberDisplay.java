ff36b16d5ac0bb6bf4b99f7ab0ba788d14337fbd
abhiakula10 <abhinavraj8258@gmail.com>
2024-09-29 23:04:17 -0700

    Update NumberDisplay.java
    finished num display

--- src/main/java/NumberDisplay.java
+++ src/main/java/NumberDisplay.java
@@ -1,1 +1,56 @@
+package src.main.java;
 
+
+
+public class NumberDisplay
+{
+    
+    private int limit;
+  
+    private int value;
+    
+    
+  
+    public NumberDisplay(int rollOverLimit) {
+        limit = rollOverLimit;
+        value = 0;
+    }
+    
+    public int getLimit() {
+        return limit;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public void setLimit(int newLimit) {
+        limit = newLimit;
+    }
+    
+    public void setValue(int newValue) {
+        if ( newValue >= 0 && newValue < limit) {
+            value = newValue;
+        }
+    }
+    
+   
+    public String getDisplayValue() {
+        String output ="";
+        output = Integer.toString(value);
+        if (output.length() == 1) {
+            output = "0" + output;
+        }
+        
+        return output;
+    }
+
+   
+    public void increment() {
+        value++;
+        
+        if (value>= limit) {
+            value = 0;
+        }
+    }
+}
